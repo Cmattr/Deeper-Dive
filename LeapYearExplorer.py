70e88c9838c559781b9f6a96217b40fbcea0dab5
Cmattr <cmattr@outlook.com>
2024-03-12 11:13:21 -0400

    Deeper Dive

---
+++ LeapYearExplorer.py
@@ -0,0 +1,25 @@
+# Task 1: Leap year Checker
+
+year =int (input("what year would you like to check"))
+century = year // 100 +1
+if year % 4 == 0:
+    print (f"{year} is a leap year")
+else:
+    print (f"{year} year is not a leap year")
+
+
+# Task 2: Century Varification
+    
+Century = year // 100 +1
+print (f"{year} is in the {Century} century")
+
+# Task 3: Time Traveler
+
+if year > 2024:
+    print ("This year is in the future")
+
+elif year == 2024:
+    print ("This year is the current year")
+
+else:
+    print ("This year is in the past")
